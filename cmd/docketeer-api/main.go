d86456d7736f43eaf12a287e2e7d6b2b3e32cf21
LUC4SNUN3S <lucascedoc@hotmail.com>
2024-10-27 14:50:05 -0300

    add resource for create and start container

--- cmd/docketeer-api/main.go
+++ cmd/docketeer-api/main.go
@@ -1,7 +1,12 @@
 package main
 
-import "dockeeter-API/internal/server"
+import (
+	"dockeeter-API/internal/server"
+	"fmt"
+)
 
 func main() {
+
 	server.Initialize()
+	fmt.Printf("Listen in port 3000")
 }
